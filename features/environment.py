2e031d1f2bbacb085d569aa693232fed908a0703
murugan anna <vishalwin@gmail.com>
2026-05-09 10:10:41 +0530

    selenium exception handling mechanism

--- features/environment.py
+++ features/environment.py
@@ -9,7 +9,7 @@ def before_scenario(context, scenario):
     )
     context.driver = webdriver.Chrome()
     context.driver.maximize_window()
-    context.driver.implicitly_wait(5)
+    # context.driver.implicitly_wait(1)
     
     
 def after_scenario(context, scenario):
--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,7 +1,8 @@
 from behave import given, when, then
 import time
 from utils.json_reader import read_json
-    
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException 
 @given(u'user launches browser')
 def step_impl(context):
     context.driver.get("https://www.saucedemo.com/")
@@ -21,7 +22,12 @@ def step_impl(context):
     
 @then("user should navigate to home page")
 def step_impl(context):
-    time.sleep(10)
-    assert "inventory" in context.driver.current_url
-    
+    try:
+        product_locator=(By.XPATH,"//span[text()='Products']")
+        product_element=context.driver.find_element(*product_locator)
+        assert product_element.is_displayed(),"Product is not displayed"
+    except NoSuchElementException:
+        # self healing code
+            assert False ,"product pae is not visisble"
+  # adding exception handling mechanism in selenium web automation      
 
